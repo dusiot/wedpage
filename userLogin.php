a6b591762e86413595ebb333ac028916b0182380
TheToma12 <143978285+TheToma12@users.noreply.github.com>
2025-01-01 22:40:48 +0800

    Update userLogin.php

--- userLogin.php
+++ userLogin.php
@@ -1,27 +1,25 @@
 <?php
 session_start();
 
-$con = mysqli_connect("localhost", "root", "", "projectevent") or die("Unable to connect to server".mysqli_connect_error());
+$con = mysqli_connect("localhost", "root", "", "projectevent") or die("Unable to connect to server: " . mysqli_connect_error());
 
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$sql = "Select * from usertable where Username='$username'";
-$result = mysqli_query($con,$sql);
+$sql = "SELECT ID, Username, Password FROM usertable WHERE Username = '$username'";
+$result = mysqli_query($con, $sql);
 
-if(mysqli_num_rows($result)== 0)
+if (mysqli_num_rows($result) == 0) {
     echo "Username does not exist";
-
-else
-{
-    $data=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if($data['Password']==$password)
-    {
-            $_SESSION["userid"]= $username;
-            header("Location:user_dashboard.php");
-            exit();
-    }
-    else
+} else {
+    $data = mysqli_fetch_assoc($result);
+    if ($data['Password'] == $password) {
+        $_SESSION['userid'] = $data['ID']; // Set session to user ID
+        echo "User  ID stored in session: " . $_SESSION['userid']; // Debugging
+        header("Location: user_dashboard.php");
+        exit();
+    } else {
         echo "Password is wrong";
+    }
 }
 ?>
